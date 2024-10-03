255adadbcce302ef5a4f77473b924eafa8c5c24a
vsonmez <hvsonmez@gmail.com>
2024-10-03 17:21:38 +0300

    welcome messages added

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,27 @@ import MessageListComponent from "./message/message-list.component";
 import useTimeTrackerWithReward from "./custom-hooks/use-time-tracker-with-reawrd.hook";
 import FooterComponent from "./ui/footer/footer.component";
 import LocationInfoComponent from "./travel/location-info.component";
+import useMessageStore from "./store/hooks/use-message-store.hook";
 
 function App() {
+  const { addMessage } = useMessageStore();
   useTimeTrackerWithReward();
+
+  React.useEffect(() => {
+    addMessage({
+      text: "I promise you there will be no ads in this game!",
+      type: "warning",
+    });
+    addMessage({
+      text: "Wiki pages will be added soon.",
+      type: "success",
+    });
+    addMessage({
+      text: "Welcome to your adventure. Good luck!",
+      type: "success",
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="h-screen flex flex-col m-auto">
